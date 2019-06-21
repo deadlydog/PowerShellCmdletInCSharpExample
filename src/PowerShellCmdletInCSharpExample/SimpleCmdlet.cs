2360a80cf6ee30d7be294b6e1743a8fc4b74c072
Daniel Schroeder <deadlydog@hotmail.com>
2019-06-20 20:27:19 -0600

    Add PowerShellStandard.Library NuGet package and a couple simple cmdlets.

---
+++ src/PowerShellCmdletInCSharpExample/SimpleCmdlet.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+
+namespace PowerShellCmdletInCSharpExample
+{
+	[Cmdlet(VerbsCommon.Get, "RepeatedString")]
+	[OutputType(typeof(string))]
+	public class GetRepeatedStringCmdlet : Cmdlet
+	{
+		[Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+		[Alias("Word")]
+		[ValidateNotNullOrEmpty()]
+		public string Phrase { get; set; }
+
+		[Parameter(Position = 1, Mandatory = true, ValueFromPipelineByPropertyName = true)]
+		[Alias("Repeat")]
+		public int NumberOfTimesToRepeatString { get; set; }
+
+		protected override void ProcessRecord()
+		{
+			base.ProcessRecord();
+
+			var result = new StringBuilder();
+			for (int i = 0; i < NumberOfTimesToRepeatString; i++)
+			{
+				result.AppendLine(Phrase);
+			}
+
+			WriteObject(result.ToString());
+		}
+	}
+
+	[Cmdlet(VerbsCommon.Get, "RepeatedStringCollection")]
+	[OutputType(typeof(IEnumerable<string>))]
+	public class GetRepeatedStringCollectionCmdlet : Cmdlet
+	{
+		[Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+		[Alias("Word")]
+		[ValidateNotNullOrEmpty()]
+		public string Phrase { get; set; }
+
+		[Parameter(Position = 1, Mandatory = true, ValueFromPipelineByPropertyName = true)]
+		[Alias("Repeat")]
+		public int NumberOfTimesToRepeatString { get; set; }
+
+		protected override void ProcessRecord()
+		{
+			base.ProcessRecord();
+
+			var results = new List<string>();
+			for (int i = 0; i < NumberOfTimesToRepeatString; i++)
+			{
+				results.Add(Phrase);
+			}
+
+			WriteObject(results, true);
+		}
+	}
+}
